901afe1d53662f245155ce3f408ed96bdfb603b4
YanZhiwei <Yan.Zhiwei@hotmail.com>
2020-10-13 00:29:23 +0800

    添加项目文件。

---
+++ XiangJiang.Logging.EventLog/EventLogService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using XiangJiang.Common;
+using XiangJiang.Logging.Abstractions;
+
+namespace XiangJiang.Logging.EventLog
+{
+    public sealed class EventLogService : ILogService
+    {
+        private readonly string _eventSource;
+        private readonly string _logName = "Application";
+
+        public EventLogService(string eventSource)
+        {
+            _eventSource = eventSource ?? throw new ArgumentNullException(nameof(_eventSource));
+            if (!System.Diagnostics.EventLog.SourceExists(eventSource))
+                System.Diagnostics.EventLog.CreateEventSource(eventSource, _logName);
+        }
+
+        public EventLogService() : this("Application")
+        {
+        }
+
+        public void Debug(string message)
+        {
+            CreateEventLog(message, EventLogEntryType.SuccessAudit);
+        }
+
+        public void Debug(string message, Exception ex)
+        {
+            CreateEventLog(message, EventLogEntryType.SuccessAudit, ex);
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void Error(string message)
+        {
+            CreateEventLog(message, EventLogEntryType.Error);
+        }
+
+        public void Error(string message, Exception ex)
+        {
+            CreateEventLog(message, EventLogEntryType.Error, ex);
+        }
+
+        public void Fatal(string message)
+        {
+            CreateEventLog(message, EventLogEntryType.FailureAudit);
+        }
+
+        public void Fatal(string message, Exception ex)
+        {
+            CreateEventLog(message, EventLogEntryType.FailureAudit, ex);
+        }
+
+        public void Info(string message)
+        {
+            CreateEventLog(message, EventLogEntryType.Information);
+        }
+
+        public void Info(string message, Exception ex)
+        {
+            CreateEventLog(message, EventLogEntryType.Information, ex);
+        }
+
+        public void Warn(string message)
+        {
+            CreateEventLog(message, EventLogEntryType.Warning);
+        }
+
+        public void Warn(string message, Exception ex)
+        {
+            CreateEventLog(message, EventLogEntryType.Warning, ex);
+        }
+
+        private void CreateEventLog(string message, EventLogEntryType logEntryType, Exception ex = null)
+        {
+            string logContent;
+
+            if (ex != null)
+            {
+                var exceptionMsg = ex.Format(message, true);
+                var builder = new StringBuilder();
+                builder.AppendLine(exceptionMsg.UserMessage);
+                builder.AppendLine(exceptionMsg.ActualMessage);
+                builder.AppendLine(exceptionMsg.StackTrace);
+                logContent = builder.ToString();
+            }
+            else
+            {
+                logContent = message;
+            }
+
+            using (var log = new System.Diagnostics.EventLog(_logName)
+            {
+                Source = _eventSource
+            })
+            {
+                log.WriteEntry(logContent, logEntryType, Thread.CurrentThread.ManagedThreadId);
+            }
+        }
+    }
+}
---
+++ XiangJiang.Logging.EventLogTests/EventLogServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XiangJiang.Logging.Abstractions;
+using XiangJiang.Logging.EventLog;
+
+namespace XiangJiang.Logging.EventLogTests
+{
+    [TestClass]
+    public class EventLogServiceTests
+    {
+        private readonly ILogService _logService;
+
+        public EventLogServiceTests()
+        {
+            _logService = new EventLogService();
+        }
+
+        [TestMethod]
+        public void ErrorTest()
+        {
+            _logService.Error("hello error");
+            try
+            {
+                throw new ArgumentNullException("hello exception");
+            }
+            catch (Exception ex)
+            {
+                _logService.Error("EventLogServiceTests", ex);
+            }
+        }
+    }
+}
